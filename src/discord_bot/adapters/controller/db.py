9b4509a5c94d45de51a292d460c26ea53083c376
zereram <emirkeser98@gmail.com>
2025-11-21 11:25:15 +0100

    added database test

--- src/discord_bot/adapters/controller/db.py
+++ src/discord_bot/adapters/controller/db.py
@@ -1,1 +1,21 @@
+from pymongo import MongoClient
 
+Mongo_URI = "mongodb+srv://admin:<db_password>@discord-bot.ps1eipx.mongodb.net/"
+DB_NAME = "discord_bot_db"#
+
+client = MongoClient(Mongo_URI)
+db= client[DB_NAME]
+
+users_collection = db["users"]
+servers_collection = db["servers"]
+command_logs_collection = db["command_logs"]
+funfacts_collection = db["funfacts"]
+auto_translate_collection = db["auto_translate"]
+dishes_collection = db["dishes"]
+
+
+
+users_collection.insert_one({
+    "user_id": 123456789,
+    "username": "example_user",
+    "role": "member"})
