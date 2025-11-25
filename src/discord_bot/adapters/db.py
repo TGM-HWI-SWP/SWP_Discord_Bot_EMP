db6ff30ac51d05abe32cff1d190c22b31a427294
zereram <emirkeser98@gmail.com>
2025-11-25 12:11:06 +0100

    Ratschiner Code

--- src/discord_bot/adapters/db.py
+++ src/discord_bot/adapters/db.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
+# from contracts.ports import DatabasePort
 
-Mongo_URI = "mongodb+srv://admin:<db_password>@discord-bot.ps1eipx.mongodb.net/"
-DB_NAME = "discord_bot_db"#
+Mongo_URI = "mongodb://localhost:27017/" # "mongodb+srv://admin:<db_password>@discord-bot.ps1eipx.mongodb.net/"
+DB_NAME = "Test" # "discord_bot_db"#
 
 client = MongoClient(Mongo_URI)
 db= client[DB_NAME]
@@ -19,3 +20,23 @@ users_collection.insert_one({
     "user_id": 123456789,
     "username": "example_user",
     "role": "member"})
+
+funfacts_collection.insert_one({
+    "id": 1 ,  
+    "Text": "Did you know? The Eiffel Tower can be 15 cm taller during the summer, due to the expansion of iron in the heat."})
+
+
+
+# class MongoDBAdapter(DatabasePort):
+#     def get_user(self, user_id: int) -> dict | None:
+#         return users_collection.find_one({"user_id": user_id})
+
+#     def add_user(self, user_data: dict) -> None:
+#         users_collection.insert_one(user_data)
+
+#     def log_command(self, command_data: dict) -> None:
+#         command_logs_collection.insert_one(command_data)
+
+#     def get_fun_fact(self, fact_id: int) -> dict | None:
+#         return funfacts_collection.find_one({"id": fact_id})
+    
