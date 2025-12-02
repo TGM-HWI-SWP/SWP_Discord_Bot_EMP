1c6e127ddcadd721a04e86d65b5a3093e70b2bd0
PHinterbauer <phinterbauer.privat@gmail.com>
2025-12-02 16:39:28 +0100

    find DISCROD mistake

--- src/discord_bot/business_logic/discord_logic.py
+++ src/discord_bot/business_logic/discord_logic.py
@@ -3,9 +3,14 @@ from discord_bot.init.config_loader import DiscordConfig
 
 client = discord.Client(intents=discord.Intents.default())
 
+TARGET_USER_ID = 577154967740219403  # Hier die User ID eingeben
+
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}')
+    
+    user = await client.fetch_user(TARGET_USER_ID)
+    await user.send("Oida, griass di! I bin der LeberkasLarry, dein persönlicher Jausen-Buddy aus’m schönen Österreich! 😎 Ob du Hunger auf a bissl Schmäh, a witzigen Fun-Fact oder a richtig g’scheite Portion Leberkas brauchst – i steh bereit. 🥖🧡 Schmeiß di auf die Couch, klick a bissl rum und lass uns gemeinsam de Zeit versüßen – versprochen, es werd lustig und liab! Ach ja, koa Angst – i bin halb Maschine, halb Fleischlaib, 100% leiwand. 😏 Pfiat di bis gleich! Dein Larry 🍀")
 
 @client.event
 async def on_message(message):
